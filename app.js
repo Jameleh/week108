ef5a1a86b1d0850fcd256c4648b2a1faceb93c30
Your Name <Jamelehasaad@gmail.com>
2021-12-10 01:43:07 +0300

    wp

--- app.js
+++ app.js
@@ -2,9 +2,21 @@
 
 export default (express, bodyParser, createReadStream, crypto, http, CORS, writeFileSync) => {
     const app=express();
+    const wp = {
+        id: 1,
+        title: {
+            rendered: 'itmo308556'
+        }
+    }
     app
     .all('/login',r=>{
         r.res.set(CORS).send("itmo308556");
+    })
+    .all('/wordpress/', r=>{
+        r.res.set(CORS).send(wp)
+    })
+    .all('/wordpress/wp-json/wp/v2/posts/', r=>{
+        r.res.set(CORS).send([wp])
     })
     
     return app;
