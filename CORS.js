1de1c096416f07ec9a860819c139e70fe144607e
Your Name <Jamelehasaad@gmail.com>
2021-12-10 02:07:35 +0300

    wp

--- CORS.js
+++ CORS.js
@@ -1,6 +1,6 @@
 export  default{
 
-       "Access-Control-Allow-Origin": "*,https://courses.openedu.ru",
+       "Access-Control-Allow-Origin": "https://courses.openedu.ru",
         "Access-Control-Allow-Methods": "*",
         "Access-Control-Expose-Headers": "*",
         "Acess-Control-Allow-Headers":"*"
